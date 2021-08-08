086ae6f0a31e43c62d95de76d7b07e599bd54c07
henrique2m <btdavintehenrique@gmail.com>
2021-08-08 00:30:38 -0300

    fix: :sparkles: corousel overflow-x in mobile version :rocket:

--- qAtiva/frontend/src/app/components/template/slider/slider.component.ts
+++ qAtiva/frontend/src/app/components/template/slider/slider.component.ts
@@ -1,9 +1,4 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
-import {
-  NgbSlideEvent,
-  NgbSlideEventSource,
-  NgbCarousel,
-} from "@ng-bootstrap/ng-bootstrap";
+import { Component, OnInit} from "@angular/core";
 
 @Component({
   selector: "app-slider",
@@ -14,33 +9,8 @@ export class SliderComponent implements OnInit {
   images = [1, 2, 3].map(
     () => `https://picsum.photos/900/500?random&t=${Math.random()}`
   );
-  showNavigationArrows = true;
-  showNavigationIndicators = true;
-  pauseOnHover = true;
-
-  @ViewChild("mycarousel", { static: true }) carousel!: NgbCarousel;
 
   constructor() {}
 
   ngOnInit() {}
-
-  startCarousel() {
-    this.carousel.cycle();
-  }
-
-  pauseCarousel() {
-    this.carousel.pause();
-  }
-
-  moveNext() {
-    this.carousel.next();
-  }
-
-  getPrev() {
-    this.carousel.prev();
-  }
-
-  goToSlide(slide: string) {
-    this.carousel.select(slide);
-  }
 }
