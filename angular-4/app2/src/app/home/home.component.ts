07ad7bfb9add86a652b98f2957962e61a1a6fb2b
henrique2m <btdavintehenrique@gmail.com>
2021-09-01 23:53:08 -0300

    :sparkles: service concept at angular :rocket:

--- angular-4/app2/src/app/home/home.component.ts
+++ angular-4/app2/src/app/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { OffersService } from '../services/offers.service'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OffersService],
 })
-export class HomeComponent {
-  constructor() { }
+export class HomeComponent implements OnInit {
+
+  constructor(private offersService: OffersService) { }
+
+  ngOnInit(): void {
+    console.log(this.offersService.getOffers())
+  }
 }
---
+++ angular-4/app2/src/app/services/offers.service.ts
@@ -0,0 +1,6 @@
+export class OffersService {
+  public getOffers(): Array<string> {
+    let offers = ['Offers1', 'Offers2', 'Offers3'];
+    return offers;
+  }
+}
