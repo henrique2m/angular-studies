2043c26a689422062f030fa080c91d511a91f6ec
henrique2m <btdavintehenrique@gmail.com>
2021-09-20 22:15:33 -0300

    :sparkles: end of class first steps to develop with Angular :rocket:

--- capgemini-bootcamp/angular-8/course-manager/src/app/app.module.ts
+++ capgemini-bootcamp/angular-8/course-manager/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list-component';
+import { StarComponent } from './star/star.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseListComponent],
+  declarations: [AppComponent, CourseListComponent, StarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
