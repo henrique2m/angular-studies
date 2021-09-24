5840bd36363e05cabd3ba62c58dc43e180ce11d1
henrique2m <btdavintehenrique@gmail.com>
2021-09-23 23:27:14 -0300

    :sparkles: configuring routes :rocket:

--- capgemini-bootcamp/angular-8/course-manager/src/app/app.module.ts
+++ capgemini-bootcamp/angular-8/course-manager/src/app/app.module.ts
@@ -2,15 +2,44 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list-component';
+import { NavBarComponent } from './nav-bar/nav-bar-component';
+import { PageNotFound } from './pageNotFound/pageNotFound.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { StarComponent } from './star/star.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseListComponent, StarComponent, ReplacePipe],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    PageNotFound,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full',
+      },
+      {
+        path: 'courses',
+        component: CourseListComponent,
+      },
+      {
+        path: '**',
+        component: PageNotFound,
+      },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ capgemini-bootcamp/angular-8/course-manager/src/app/pageNotFound/pageNotFound.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  templateUrl: './pageNotFound.component.html',
+})
+export class PageNotFound {
+  constructor() {}
+}
