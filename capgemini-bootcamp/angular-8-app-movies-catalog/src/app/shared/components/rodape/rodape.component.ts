d8a2db0d70f59f99510f794f61ac7135bed188b1
henrique2m <btdavintehenrique@gmail.com>
2022-04-04 16:08:32 -0300

    :sparkles: finalizing movie catalog app  :rocket:

---
+++ capgemini-bootcamp/angular-8-app-movies-catalog/src/app/shared/components/rodape/rodape.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "dio-rodape",
+  templateUrl: "./rodape.component.html",
+  styleUrls: ["./rodape.component.scss"],
+})
+export class RodapeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
