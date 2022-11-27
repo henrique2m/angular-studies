e31b5d36f9b488227997c3d60bdf9da7cce6c390
henrique2m <btdavintehenrique@gmail.com>
2022-11-27 00:48:00 -0300

    test :sparkles: polishing test description  :rocket:

---
+++ tests/unit-tests- introduction/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'Angular testing';
+
+  public likesCount = 0;
+
+  public like(): void {
+    this.likesCount++;
+  }
+}
